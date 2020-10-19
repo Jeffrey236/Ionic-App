734648d95b1fe92ac2ef3a01f49466e82d5453fa
Jeffrey <jefstone@hotmail.com>
2020-10-19 17:15:51 +0200

    galerie init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { GalerieComponent } from './galerie/galerie.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [	AppComponent,
+      GalerieComponent
+   ],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
